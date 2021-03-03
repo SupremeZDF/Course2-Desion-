80f04f3a9302c88a48a28d5ab58ca0e55af557f4
东风 赵 <zdf@cheok.com>
2021-03-03 17:45:27 +0800

    代码提交

--- DesinSixtenet/Form1.Designer.cs
+++ DesinSixtenet/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(79, 88);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 48);
+            this.button1.Size = new System.Drawing.Size(72, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,10 +45,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 450);
+            this.ClientSize = new System.Drawing.Size(636, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- DesinSixtenet/Form1.cs
+++ DesinSixtenet/Form1.cs
@@ -19,7 +19,24 @@ namespace DesinSixtenet
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            var c = AA.bb;
+            c.Age = 12;
+            c.Name = "hah1";
+            var dd = AA.bb;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class AA
+    {
+        public static AA bb = new AA() { Name = "123", Age = 123 };
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
