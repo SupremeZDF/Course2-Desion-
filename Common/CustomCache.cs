19b04a7f387673567686220bf0d25e93e425ec31
Z D F <2733932691@qq.com>
2021-03-11 23:21:02 +0800

    代码提交

---
+++ Common/CustomCache.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// 第三方数据存储和获取的地方
+    /// 
+    /// 过期策略：
+    /// 永久有效---目前就是
+    /// 绝对过期---有个时间点，超过就过期
+    /// 滑动过期---多久之后过期，如果期间更新/查询/检查存在，就再次延长
+    /// 
+    /// 主动清理+被动清理，保证过期数据不会被查询；过期数据也不会滞留太久
+    /// 
+    /// 多线程操作非线程安全的容器，会造成冲突
+    /// 1 线程安全容器ConcurrentDictionary
+    /// 2 用lock---Add/Remove/遍历 解决问题了，但是性能怎么办呢
+    ///   怎么降低影响，提升性能呢？
+    ///   多个数据容器，多个锁，容器之间可以并发
+    /// </summary>
+    public class CustomCache //: ICache
+    {
+        //ConcurrentDictionary
+
+        private static readonly object CustomCache_Lock = new object();
+
+        /// <summary>
+        /// 主动清理
+        /// </summary>
+        static CustomCache()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        List<string> keyList = new List<string>();
+                        lock (CustomCache_Lock)
+                        {
+                            foreach (var key in CustomCacheDictionary.Keys)
+                            {
+                                DataModel model = (DataModel)CustomCacheDictionary[key];
+                                if (model.ObsloteType != ObsloteType.Never && model.DeadLine < DateTime.Now)
+                                {
+                                    keyList.Add(key);
+                                }
+                            }
+                            keyList.ForEach(s => Remove(s));
+                        }
+                        Thread.Sleep(1000 * 60 * 10);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// private:私有一下数据容器，安全
+        /// static：不被GC
+        ///   字典：读写效率高
+        /// </summary>
+        //private static Dictionary<string, object> CustomCacheDictionary = new Dictionary<string, object>();
+
+        private static Dictionary<string, object> CustomCacheDictionary = new Dictionary<string, object>();
+
+        public static void Add(string key, object oVaule)
+        {
+            lock (CustomCache_Lock)
+                CustomCacheDictionary.Add(key, new DataModel()
+                {
+                    Value = oVaule,
+                    ObsloteType = ObsloteType.Never,
+                });
+        }
+        /// <summary>
+        /// 绝对过期
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="oVaule"></param>
+        /// <param name="timeOutSecond"></param>
+        public static void Add(string key, object oVaule, int timeOutSecond)
+        {
+            lock (CustomCache_Lock)
+                CustomCacheDictionary.Add(key, new DataModel()
+                {
+                    Value = oVaule,
+                    ObsloteType = ObsloteType.Absolutely,
+                    DeadLine = DateTime.Now.AddSeconds(timeOutSecond)
+                });
+        }
+        /// <summary>
+        /// 相对过期
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="oVaule"></param>
+        /// <param name="duration"></param>
+        public static void Add(string key, object oVaule, TimeSpan duration)
+        {
+            lock (CustomCache_Lock)
+                CustomCacheDictionary.Add(key, new DataModel()
+                {
+                    Value = oVaule,
+                    ObsloteType = ObsloteType.Relative,
+                    DeadLine = DateTime.Now.Add(duration),
+                    Duration = duration
+                });
+        }
+
+        /// <summary>
+        /// 要求在Get前做Exists检测
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T Get<T>(string key)
+        {
+            return (T)(((DataModel)CustomCacheDictionary[key]).Value);
+        }
+
+        /// <summary>
+        /// 被动清理，请求了数据，才能清理
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool Exists(string key)
+        {
+            if (CustomCacheDictionary.ContainsKey(key))
+            {
+                DataModel model = (DataModel)CustomCacheDictionary[key];
+                if (model.ObsloteType == ObsloteType.Never)
+                {
+                    return true;
+                }
+                else if (model.DeadLine < DateTime.Now)//现在已经超过你的最后时间
+                {
+                    lock (CustomCache_Lock)
+                        CustomCacheDictionary.Remove(key);
+                    return false;
+                }
+                else
+                {
+                    if (model.ObsloteType == ObsloteType.Relative)//没有过期&是滑动 所以要更新
+                    {
+                        model.DeadLine = DateTime.Now.Add(model.Duration);
+                    }
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 删除key
+        /// </summary>
+        /// <param name="key"></param>
+        public static void Remove(string key)
+        {
+            lock (CustomCache_Lock)
+                CustomCacheDictionary.Remove(key);
+        }
+
+        public static void RemoveAll()
+        {
+            lock (CustomCache_Lock)
+                CustomCacheDictionary.Clear();
+        }
+        /// <summary>
+        /// 按条件删除
+        /// </summary>
+        /// <param name="func"></param>
+        public static void RemoveCondition(Func<string, bool> func)
+        {
+            List<string> keyList = new List<string>();
+            lock (CustomCache_Lock)
+                foreach (var key in CustomCacheDictionary.Keys)
+                {
+                    if (func.Invoke(key))
+                    {
+                        keyList.Add(key);
+                    }
+                }
+            keyList.ForEach(s => Remove(s));
+        }
+
+        public static T GetT<T>(string key, Func<T> func)
+        {
+            T t = default(T);
+            if (!CustomCache.Exists(key))
+            {
+                t = func.Invoke();
+                CustomCache.Add(key, t);
+            }
+            else
+            {
+                t = CustomCache.Get<T>(key);
+            }
+            return t;
+        }
+    }
+
+    public class CustomCacheNew
+    {
+        //动态初始化多个容器和多个锁
+        private static int CPUNumer = 0;//获取系统的CPU数
+        private static List<Dictionary<string, object>> DictionaryList = new List<Dictionary<string, object>>();
+        private static List<object> LockList = new List<object>();
+        static CustomCacheNew()
+        {
+            CPUNumer = 4;
+            for (int i = 0; i < CPUNumer; i++)
+            {
+                DictionaryList.Add(new Dictionary<string, object>());
+                LockList.Add(new object());
+            }
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(1000 * 60 * 10);
+                    try
+                    {
+                        for (int i = 0; i < CPUNumer; i++)
+                        {
+                            List<string> keyList = new List<string>();
+                            lock (LockList[i])//减少锁的影响范围
+                            {
+                                foreach (var key in DictionaryList[i].Keys)
+                                {
+                                    DataModel model = (DataModel)DictionaryList[i][key];
+                                    if (model.ObsloteType != ObsloteType.Never && model.DeadLine < DateTime.Now)
+                                    {
+                                        keyList.Add(key);
+                                    }
+                                }
+                                keyList.ForEach(s => DictionaryList[i].Remove(s));
+                            }
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+                }
+            });
+        }
+
+        //假如给你个key，你怎么知道在哪里？ 需要多一个规则
+        public static void Add(string key, object oValue)
+        {
+            int hash = key.GetHashCode();//相对均匀而且稳定
+            int index = hash % CPUNumer;
+            lock (LockList[index])
+            {
+                DictionaryList[index].Add(key, oValue);
+            }
+        }
+
+        public static void Remove(string key)
+        {
+            int hash = key.GetHashCode();//相对均匀而且稳定
+            int index = hash % CPUNumer;
+            lock (LockList[index])
+            {
+                DictionaryList[index].Remove(key);
+            }
+        }
+
+
+
+    }
+
+    /// <summary>
+    /// 缓存的信息
+    /// </summary>
+    internal class DataModel
+    {
+        public object Value { get; set; }
+        public ObsloteType ObsloteType { get; set; }
+        public DateTime DeadLine { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        //数据清理后出发事件
+        public event Action DataClearEvent;
+    }
+
+    public enum ObsloteType
+    {
+        Never,
+        Absolutely,
+        Relative
+    }
+}
---
+++ MyCache/Program.cs
@@ -0,0 +1,216 @@
+﻿using Common;
+using Ruanmou.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyCache
+{
+    /// <summary>
+    /// 1 客户端缓存-CDN缓存-反向代理缓存-本地缓存
+    /// 2 本地缓存原理和手写基础实现
+    /// 
+    /// 系统性能优化的第一步，就是使用缓存
+    /// 缓存：实际上是一种效果&目标，就是获取数据的时候，第一次获取之后找个地方存起来，后面直接用，这样一来可以提升后面每次获取数据的效率
+    /// 读取配置文件的时候把信息放在静态字段，这个就是缓存
+    /// 缓存是无所不在的。。
+    /// 
+    /// 
+    /// 1 缓存数据更新和过期策略
+    /// 2 多线程冲突与解决 
+    /// 3 缓存类库封装和缓存应用总结
+    /// 
+    /// 
+    /// 缓存究竟哪里用？ 满足哪些特点适合用缓存？
+    /// 访问频繁+耗时耗资源+相对稳定+体积不那么大
+    /// 不是说严格满足，看情况，存一次能查3次，就值得缓存(大型项目标准)
+    /// 字典/省市区/配置文件
+    /// 公告信息/部门/权限/用户
+    /// 热搜/类别列表/产品列表
+    /// 
+    /// 股票价格数据/彩票开奖信息  不行，即时性要求很高
+    /// 图片/视频   不行，体积太大
+    /// 商品评论  可以的，虽然评论会变，但是这不重要，而且第一页一般不变
+    /// 
+    /// 
+    /// 可以测试一下CustomCache的性能，十万/百万/千万 插入&获取&删除的性能
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("欢迎来到.net高级班vip课程，今天是Eleven老师为大家带来的缓存Cache");
+                {
+                    #region MyRegion
+                    ////1 重复请求--100人访问首页--每个人做的其实都一样--不就是重复
+                    ////2 耗时/耗资源
+                    ////3 结果没变
+                    ////应该缓存一下，来提升性能
+                    //Console.WriteLine("********************************");
+                    //for (int i = 0; i < 5; i++)
+                    //{
+                    //    Console.WriteLine($"获取{nameof(DBHelper)} {i}次 {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}");
+                    //    //List<Program> programList = DBHelper.Query<Program>(123);
+                    //    List<Program> programList = null;
+                    //    string key = $"{nameof(DBHelper)}_Query_{123}";
+                    //    //存取数据的唯一标识:1 唯一的  2 能重现
+                    //    //if (!CustomCache.Exists(key))
+                    //    //{
+                    //    //    programList = DBHelper.Query<Program>(123);
+                    //    //    CustomCache.Add(key, programList);
+                    //    //}
+                    //    //else
+                    //    //{
+                    //    //    programList = CustomCache.Get<List<Program>>(key);
+                    //    //}
+                    //    programList = CustomCache.GetT<List<Program>>(key, () => DBHelper.Query<Program>(123));
+                    //}
+                    //Console.WriteLine("********************************");
+                    //for (int i = 0; i < 5; i++)
+                    //{
+                    //    Console.WriteLine($"获取{nameof(FileHelper)} {i}次 {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}");
+                    //    //List<Program> programList = FileHelper.Query<Program>(234);
+                    //    List<Program> programList = null;
+                    //    string key = $"{nameof(FileHelper)}_Query_{234}";
+                    //    //存取数据的唯一标识:1 唯一的  2 能重现
+                    //    //if (!CustomCache.Exists(key))
+                    //    //{
+                    //    //    programList = FileHelper.Query<Program>(234);
+                    //    //    CustomCache.Add(key, programList);
+                    //    //}
+                    //    //else
+                    //    //{
+                    //    //    programList = CustomCache.Get<List<Program>>(key);
+                    //    //}
+                    //    programList = CustomCache.GetT<List<Program>>(key, () => FileHelper.Query<Program>(234));
+                    //}
+                    //Console.WriteLine("********************************");
+                    //for (int i = 0; i < 5; i++)
+                    //{
+                    //    Console.WriteLine($"获取{nameof(RemoteHelper)} {i}次 {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}");
+                    //    //List<Program> programList = RemoteHelper.Query<Program>(345);
+                    //    List<Program> programList = null;
+                    //    string key = $"{nameof(RemoteHelper)}_Query_{345}";
+                    //    //存取数据的唯一标识:1 唯一的  2 能重现
+                    //    //if (!CustomCache.Exists(key))
+                    //    //{
+                    //    //    programList = RemoteHelper.Query<Program>(345);
+                    //    //    CustomCache.Add(key, programList);
+                    //    //}
+                    //    //else
+                    //    //{
+                    //    //    programList = CustomCache.Get<List<Program>>(key);
+                    //    //}
+                    //    programList = CustomCache.GetT<List<Program>>(key, () => RemoteHelper.Query<Program>(345));
+                    //}
+                    #endregion
+                }
+                {
+                    //for (int i = 0; i < 5; i++)
+                    //{
+                    //    Console.WriteLine($"获取{nameof(DBHelper)} {i}次 {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}");
+                    //    //List<Program> programList = DBHelper.Query<Program>(123);
+
+                    //    List<Program> programList = null;
+                    //    string key = $"{nameof(DBHelper)}_Query_{123}";
+                    //    //存取数据的唯一标识:1 唯一的  2 能重现
+                    //    if (!CustomCache.Exists(key))
+                    //    {
+                    //        programList = DBHelper.Query<Program>(123);
+                    //        CustomCache.Add(key, programList);
+                    //    }
+                    //    else
+                    //    {
+                    //        programList = CustomCache.Get<List<Program>>(key);
+                    //    }
+                    //}
+                    ////缓存优化性能，核心就是结果重用，下一次请求还是用上一次的结果
+                    ////如果数据有变化怎么办呢？ 岂不是用了一个错误的数据？ 
+                    ////是的。。缓存难免会有脏数据， 当然了，我们也会分门别类去尽量减少脏数据
+                    //{
+                    //    //用户-角色-菜单  用户权限查的多+比较耗资源+相对稳定，非常适合缓存，缓存方式应该是用户id为key，菜单列表为value
+                    //    string name = "Eleven_menu__Menu";
+                    //    List<string> menu = new List<string>();
+                    //    if (!CustomCache.Exists(name))
+                    //    {
+                    //        menu = new List<string>() { "123", "125553", "143", "123456" };
+                    //        CustomCache.Add(name, menu);
+                    //    }
+                    //    else
+                    //    {
+                    //        menu = CustomCache.Get<List<string>>(name);
+                    //    }
+                    //}
+                    //{
+                    //    //1 Eleven的权限变化了,缓存应该失效
+                    //    //数据更新影响单条缓存，常规做法是Remove掉 而不是更新
+                    //    //因为缓存只是用来提升效率的，而不是数据保存的，因此的不需要更新，只需要删除就好，如果真的下次用上了，到时候再去初始化
+                    //    string name = "Eleven_menu__Menu";
+                    //    CustomCache.Remove(name);
+
+                    //    List<string> menu = new List<string>();
+                    //    if (!CustomCache.Exists(name))
+                    //    {
+                    //        menu = new List<string>() { "123", "125553", "143" };
+                    //        CustomCache.Add(name, menu);
+                    //    }
+                    //    else
+                    //    {
+                    //        menu = CustomCache.Get<List<string>>(name);
+                    //    }
+                    //}
+                    //{
+                    //    //2 删除了某个菜单，影响了一大批用户
+
+                    //    //全部干掉，殃及池鱼(最模糊)
+                    //    CustomCache.RemoveAll();
+
+                    //    //根据菜单--找角色--找用户--每一个拼装key然后去Remove(最精准)
+                    //    //这个不行，为了缓存增加数据库的任务，最大的问题是数据量的问题，缓存是二八原则，只有20%的热点用户才缓存，像这样成本太高
+
+                    //    //菜单删除了，能不能只影响一部分的缓存数据
+                    //    //1 添加缓存时，key带上规则，比如权限包含_menu_
+                    //    //2 清理时就只删除key含_menu_的
+                    //    CustomCache.RemoveCondition(s => s.Contains("_menu_"));
+                    //}
+                    //{
+                    //    //3 第三方修改了数据，缓存并不知道，这个没办法
+                    //    //a 可以调用接口清理缓存，b系统修改数据，调用c系统通知下缓存更新
+                    //    //b 就只能容忍了。容忍脏数据。但是可以加上时间限制，减少影响时间
+                    //}
+                    {
+                        //多线程问题
+                        List<Task> taskList = new List<Task>();
+                        for (int i = 0; i < 1_000_000; i++)
+                        {
+                            int k = i;
+                            taskList.Add(Task.Run(() => CustomCache.Add($"TestKey_{k}", $"TestValue_{k}", 10)));
+                        }
+                        for (int i = 0; i < 100; i++)
+                        {
+                            int k = i;
+                            taskList.Add(Task.Run(() => CustomCache.Remove($"TestKey_{k}")));
+                        }
+                        for (int i = 0; i < 100; i++)
+                        {
+                            int k = i;
+                            taskList.Add(Task.Run(() => CustomCache.Exists($"TestKey_{k}")));
+                        }
+                        //Thread.Sleep(10*1000);
+                        Task.WaitAll(taskList.ToArray());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
